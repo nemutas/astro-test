4925e723441ceff7c19b90cee8c4ad012103bec6
nemutas <inohiro.focru@gmail.com>
2023-01-26 00:40:18 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nemutas.github.io',
-	base: '/astro-test',
+	// base: '/astro-test',
+	vite: ({ mode }) => ({
+		base: mode === 'development' ? '/' : '/astro-test/',
+	}),
 })
